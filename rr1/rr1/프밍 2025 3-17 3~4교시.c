25b3157a7bbde468b937bf80d868699dffaac631
d1ng1724 <s25004@gsm.hs.kr>
2025-03-18 18:13:46 +0900

    프밍 2025.3.17 3,4교시
    입출력함수 배웠던 것 같음

---
+++ rr1/rr1/프밍 2025 3-17 3~4교시.c
@@ -0,0 +1,69 @@
+﻿// part 2, 02 입출력함수
+/*#include <stdio.h>
+
+int main(){
+    printf_s("\n*****13번*****\n"); // 13
+    int a = 1234;
+    float b = 1.235;
+    printf("%d\n", a);
+    printf("%10d\n", a);
+    printf("%f\n", b);
+    printf("%9f\n", b);
+    printf("%9.2f\n", b);
+
+
+
+    printf_s("\n*****14번*****\n"); // 14
+    putchar('H');
+    putchar('a');
+    putchar('p');
+    putchar('p');
+    putchar('y');
+
+    putchar('\a');
+    printf_s("h\n");
+    putchar('h');
+    putchar(98);
+
+
+
+    printf_s("\n*****15번*****\n"); // 15
+    puts("Happy");
+    puts("Happy\n");
+    puts("X-mas\a");
+    puts("\tGood friend");
+
+
+
+    printf_s("\n*****16번*****\n"); // 16
+    int x, y;
+
+    x = 3; //scanf_s("%d", &x);
+    y = 5; //scanf_s("%d", &y);
+    printf("%d+%d=%d", x, y, x+y);
+
+
+
+    printf_s("\n*****17번*****\n"); // 17
+    char ch;
+
+    printf_s("1개의 영문자를 입력 : ");
+    ch = 'd'; //ch = getchar();
+    printf("Typed : %c\n", ch);
+
+
+
+    printf_s("\n*****18번*****\n"); // 18
+    char ch_arr[20];
+    printf_s("이름을 영문으로 적으세요 : ");
+    gets(ch_arr);
+    printf_s("ch_arr = %s\n", ch_arr);
+
+
+
+    printf_s("\n*****ㅇㅇㅇ*****\n"); // 몇학년몇반번호
+    int grade, clas2, num;
+
+    scanf_s("%d %d %d", &grade, &clas2, &num);
+    printf_s("%d학년 %d반 %d번 홍길동", grade, clas2, num);
+}*/
